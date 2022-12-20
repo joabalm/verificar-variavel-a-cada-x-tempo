271fa831ab4aa0da551d283cb590cd549bd078d1
joabalm <99767947+joabalm@users.noreply.github.com>
2022-12-20 08:50:41 -0300

    Add files via upload

---
+++ verificar_tempo.py
@@ -0,0 +1,36 @@
+import datetime as dt
+import requests
+import json
+
+hora_inicio = dt.datetime.now()
+hora_igual = dt.datetime.now()
+var1 = "cachorro"
+var2 = "cachorro"
+
+def comparar(var1, var2):
+    if var1 == var2:
+        return "são iguais"
+    else:
+        return "são diferentes"
+
+def post(var1, hora_fim):
+    API_ENDPOINT = "http://localhost:3000/dados"
+    API_KEY = "b&&$2WoZ)1cuTEE@s2o"
+    dados = {
+        'key': API_KEY,
+        'var': var1,
+        'hora': hora_fim
+    }
+    r = requests.post(url=API_ENDPOINT, data=dados)
+    pastebin_url = r.text
+    return "The pastebin URL is:%s" % pastebin_url
+
+while True:
+    hora_fim = hora_inicio + dt.timedelta(seconds=10)
+    if hora_igual.ctime() == hora_fim.ctime():
+        print(comparar(var1, var2))
+        hora_inicio = dt.datetime.now()
+        print(post(var1, hora_fim))
+    else:
+        hora_igual = dt.datetime.now()
+        # print(".", hora_inicio, hora_fim, hora_i
